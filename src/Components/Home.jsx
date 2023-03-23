a3b3ed8bf84eefa528e98787ce6eb55649f3bf41
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-23 22:23:10 +0530

    practice

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Movies from "./Movies";
 import Search from "./Search";
-import "./home.css"
-
+import "./home.css";
 
 const Home = () => {
-  
   return (
     <div className="mainContainer">
-    <header className="header">
-      <h1>home</h1> 
-    </header>
-      <Search/>
-      <Movies className="movies"/>
+      <header className="header">
+        <h1>home</h1>
+      </header>
+      <Search />
+      <Movies className="movies" />
     </div>
   );
 };
