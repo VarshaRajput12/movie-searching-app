e04d5294abbab278fc11c6cc2136faa13b78bab7
VarshaRajput12 <varsharajput353@gmail.com>
2023-03-23 22:23:47 +0530

    practice

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -6,25 +6,25 @@ import { AppContext } from "./context";
 
 const Movies = () => {
   const { movie, isLoading } = useContext(AppContext);
-  if (isLoading){
-    <div>Loading...</div>
+  if (isLoading) {
+    <div>Loading...</div>;
   }
-    return (
-      <div className="container">
-        {movie.map((currMovie) => {
-          const { Title, imdbID, Poster } = currMovie;
-          const newTitle = Title.substring(0, 15);
-          return (
-            <div key={imdbID} className="single-movie">
-              <NavLink to={`movie/${imdbID}`} className="NavLink">
-                <h2>{newTitle.length >= 15 ? `${newTitle}...` : Title}</h2>
-                <img src={Poster} alt="Poster" className="poster" />
-              </NavLink>
-            </div>
-          );
-        })}
-      </div>
-    );
+  return (
+    <div className="container">
+      {movie.map((currMovie) => {
+        const { Title, imdbID, Poster } = currMovie;
+        const newTitle = Title.substring(0, 15);
+        return (
+          <div key={imdbID} className="single-movie">
+            <NavLink to={`movie/${imdbID}`} className="NavLink">
+              <h2>{newTitle.length >= 15 ? `${newTitle}...` : Title}</h2>
+              <img src={Poster} alt="Poster" className="poster" />
+            </NavLink>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Movies;
